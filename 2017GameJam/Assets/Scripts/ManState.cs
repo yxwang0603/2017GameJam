8a60f0af34bb6f89c1e10f0ca4de378acb151008
yxwang0603 <yxwang0603@gmail.com>
2017-01-21 14:24:08 -0500

    state

---
+++ 2017GameJam/Assets/Scripts/ManState.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManState : MonoBehaviour {
+
+    public static AudienceState state;
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        state = AudienceState.Idle;
+        anim = transform.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        switch (state)
+        {
+            case AudienceState.Idle:
+
+                break;
+            case AudienceState.Wave:
+                break;
+            case AudienceState.Sleep:
+                
+                break;
+        }
+	}
+
+    public void SetToIdle()
+    {
+        state = AudienceState.Idle;
+    }
+
+    public void SetToSleep()
+    {
+        state = AudienceState.Sleep;
+    }
+
+    public void SetToWave()
+    {
+        state = AudienceState.Wave;
+    }
+
+    public enum AudienceState
+    {
+        Idle,
+        Wave,
+        Sleep,
+    }
+}
